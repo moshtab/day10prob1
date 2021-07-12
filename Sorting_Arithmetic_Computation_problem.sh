9d3a79b4f84b1381dee4e3d78e773c1ef8804bc1
moshtab <mdmohsin41439@gmail.com>
2021-07-12 21:47:56 +0530

    sorting in descending order

--- Sorting_Arithmetic_Computation_problem.sh
+++ Sorting_Arithmetic_Computation_problem.sh
@@ -7,5 +7,18 @@ compute[((counter++))]=$(((a*b)+c))
 compute[((counter++))]=$((c+(a/b)))
 compute[((counter++))]=$(((a%b)+c))
 echo ${compute[@]}
+for ((i=0;i<5;i++))
+do
+    for((j=$i;j<5;j++))
+    do
+       if [ ${compute[$i]} -lt ${compute[$j]} ]
+       then
+          temp=${compute[i]}
+          compute[$i]=${compute[$j]}
+          compute[$j]=$temp
+       fi
+    done
+done
+echo "descending order is " ${compute[@]}
 
 
