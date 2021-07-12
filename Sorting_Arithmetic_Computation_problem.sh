bd165dfcbf065f2c38e8b9f27dc48e9fd3a66784
moshtab <mdmohsin41439@gmail.com>
2021-07-12 21:36:10 +0530

    storing in dictionary

--- Sorting_Arithmetic_Computation_problem.sh
+++ Sorting_Arithmetic_Computation_problem.sh
@@ -1,6 +1,10 @@
+declare -A compute
 a=1
 b=2
-c=3
-compute=$((a%b+c))
-echo "solution is " $compute
+c=4
+compute[1]=$((a+(b*c)))
+compute[2]=$(((a*b)+c))
+compute[3]=$((c+(a/b)))
+compute[4]=$(((a%b)+c))
+echo ${compute[@]}
 
